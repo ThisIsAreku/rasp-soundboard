57fe11dd2b38b7ee519afcaf1b09fef8d79a4f17
Alexandre Boucey <alexandre.boucey@gmail.com>
2014-07-06 19:39:12 +0200

    Updated, read keyboard from event0

--- srv.js
+++ srv.js
@@ -3,9 +3,14 @@ const
 	path = require('path'),
 	process = require('child_process'),
 	qs = require('querystring'),
-	merge = require('merge');
+	merge = require('merge'),
+	InputEvent = require('../keybd/input-event');
 
-var api = new Api;
+const VERSION = "1.2";
+
+const api = new Api;
+const keyboard = new InputEvent('event0');
+
 const
 	soundFolder = __dirname + '/sounds/',
 	localFolder = __dirname + '/public/'
@@ -15,9 +20,22 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var soundFiles = null;
+var keybindings = {};
 
 app.get('/', function(req, res){
 	res.sendfile(localFolder + 'index.html');
+});
+app.get('/keyboard.js', function(req, res){
+	res.sendfile(localFolder + 'keyboard.js');
+});
+app.get('/getSounds.json', function(req, res){
+	res.sendfile(localFolder+'soundCache.json');
+});
+app.get('/getBindings.json', function(req, res){
+	res.sendfile(localFolder+'bindingsCache.json');
+});
+app.get('/getSound/:id(^[a-z]{0,10}$)', function(req, res){
+	res.end(JSON.stringify(req.params));
 });
 
 var playersCount = 0;
@@ -27,8 +45,8 @@ io.on('connection', function (socket) {
 	console.log("Connexion from " + clientIP);
 	players[socket.id] = clientIP;
 	playersCount++;
-	if(soundFiles != null)
-		socket.emit('getFiles', soundFiles);
+	/*if(soundFiles != null)
+		socket.emit('getFiles', soundFiles);*/
 	socket.on('playSound', function (data, fn){
 		var soundName = soundFiles[data.hash]
 		socket.broadcast.emit('startSound', { name: soundName, hash: data.hash });
@@ -43,21 +61,51 @@ io.on('connection', function (socket) {
 	});
 	socket.on('Refresh', function (data){
 		console.log('Re-Indexing..');
-		walk(soundFolder, function(err, results) {
-			if (err) throw err;
-			soundFiles = results;
-			io.emit('getFiles', soundFiles);
-		});
+		reIndexSoundFiles();
+	});
+	socket.on('add bind', function (data){
+		var keyName = data.key.toUpperCase();
+		console.log('Binding ' + keyName + ' to ' + soundFiles[data.hash]);
+		keybindings['KEY_'+keyName] = data.hash;
+		fs.writeFile(localFolder+'bindingsCache.json', JSON.stringify(keybindings));	
+	});
+	socket.on('remove bind', function (data){
+		var keyName = data.key.toUpperCase();
+		console.log('Removing binding ' + keyName);
+		delete keybindings['KEY_'+keyName];
+		fs.writeFile(localFolder+'bindingsCache.json', JSON.stringify(keybindings));
 	});
 	socket.on('PlayingStats', function (data, fn){
 		fn(api.listCurrentSounds());
 	});
-	io.emit('PlayerStats', { count: playersCount, data: players });
+	io.emit('PlayerStats', { count: playersCount, data: players, version: VERSION });
 	socket.on('disconnect', function () {
 		console.log("Disconnect from " + clientIP);
 		playersCount--;
 		delete players[socket.id];
-		io.emit('PlayerStats', { count: playersCount, data: players });
+		io.emit('PlayerStats', { count: playersCount, data: players, version: VERSION });
+	});
+	socket.on('reconnect', function () {
+		if(soundFiles != null)
+			socket.emit('getFiles', soundFiles);
+	});
+});
+
+keyboard.on('keypress', function (event){
+	if(event.keyCode == 57){
+		console.log("[KBD] -- STOP ALL -- ");
+		return api.stopAll();
+	}
+	io.emit('key pressed', event);
+	if('undefined' == typeof keybindings[event.keyName])
+		return;
+
+	var hash = keybindings[event.keyName];
+	var soundName = soundFiles[hash]
+	console.log("[KBD] startSound: "+soundName);
+	io.emit('startSound', { name: soundName, hash: hash });
+	api.playSound(hash, function (soundName){
+		io.emit('endSound', { name: soundName, hash: hash });
 	});
 });
 
@@ -89,13 +137,53 @@ var walk = function(dir, done) {
   });
 };
 
+if(fs.existsSync(localFolder+'soundCache.json')){
+	console.log('Reading sounds from cache..');
+	var filestr = fs.readFileSync(localFolder+'soundCache.json');
+	soundFiles = JSON.parse(filestr);
+	console.log('Checking cache..');
+	checkCache();
+}else{
+	console.log('Indexing..');
+	reIndexSoundFiles();
+}
 
-console.log('Indexing..');
-walk(soundFolder, function(err, results) {
-	if (err) throw err;
-	soundFiles = results;
-	io.emit('getFiles', soundFiles);
-});
+if(fs.existsSync(localFolder+'bindingsCache.json')){
+	console.log('Reading bindings from cache..');
+	var filestr = fs.readFileSync(localFolder+'bindingsCache.json');
+	keybindings = JSON.parse(filestr);
+}
+
+function reIndexSoundFiles(){
+	walk(soundFolder, function(err, results) {
+		if (err) throw err;
+		soundFiles = results;
+		console.log(Object.keys(soundFiles).length + " file(s) indexed");
+		io.emit('getFiles', soundFiles);
+		fs.writeFile(localFolder+'soundCache.json', JSON.stringify(soundFiles));
+	});
+}
+
+function checkCache(){
+	var missing = 0;
+	var remains = Object.keys(soundFiles).length;
+	for(var hash in soundFiles){
+		var file = soundFiles[hash];
+		fs.exists(soundFolder+file, function(ex){
+			if(!ex){
+				if(missing == 0)
+					reIndexSoundFiles();
+				missing++;
+				console.log(file + " is missing");
+			}
+			if(!--remains){
+				console.log("Cache checked");
+				console.log(Object.keys(soundFiles).length + " file(s) indexed");
+			}
+		})
+	}
+}
+
 
 
 http.listen(7123, function(){
